0b8b053960d75047bf6aaf281284a43d3a4f62db
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-02-03 09:37:03 -0500

    initial commit

---
+++ src/client/java/github/com/quebecmod1/client/Quebecmod1Client.java
@@ -0,0 +1,10 @@
+package github.com.quebecmod1.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Quebecmod1Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/github/com/quebecmod1/Quebecmod1.java
@@ -0,0 +1,10 @@
+package github.com.quebecmod1;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Quebecmod1 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
